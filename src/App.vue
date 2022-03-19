7b2a6c54a54d282a73373aa4412b4d7e41a93663
Yuler Figueroa <yuleralex1@gmail.com>
2022-03-18 21:59:13 -0500

    Add footer copyright

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header/>
-  <header>
-    <div class="wrapper">
-      <nav>
-
-
-      </nav>
-    </div>
-  </header>
+  <Footer/>
 
   <RouterView />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="fixed-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-8">
+          <p >Copyright © 2022 <b>FACHCODE S.A.C. </b> Todos los derechos reservados.</p>
+        </div>
+        <div class="col-4">
+          <p  class="text-end">Versión Beta</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+</template>
